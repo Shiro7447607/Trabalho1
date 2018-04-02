e9df07ca7ca6bbe26607244949e2f0b54624c9a1
Shiro7447607 <33848124+Shiro7447607@users.noreply.github.com>
2018-04-02 19:27:33 -0300

    Update trab1.c

--- trab1.c
+++ trab1.c
@@ -1,49 +1,38 @@
 /*
 Alunos-Matrícula:
-
- Marcos Nery Borges Junior - 170017885
-
-
+ Marcos Nery Borges Junior - 17/0017885
+ Matheus Salles Blanco - 16/0138400
 */
 #include<stdio.h>
 void fneuronio(int * entradas, int * pesos, int tamanho,int T, int * result){
-        
          int i,somap=0;
       
          for(i=0;i<tamanho;i++){
-             
                 somap += (entradas[i]*pesos[i]);
-
-             
              }
         
-         if(somap > T){*result=1;}
-         else{*result = 0;}
-             
-    
+         if(somap > T){
+		 	*result=1;
+		 }
+         else{
+		 	*result = 0;
+			 }
     }
 
 
 int main(){
-
-   int i, T,entradas[10],pesos[10], resultado;
+	int i, T,entradas[10],pesos[10], resultado;
  
     printf("Entre com os elementos do vetor neural \n");  
     
-    for(i=0;i<10;i++){
-        
-         
+    for(i=0;i<10;i++){    
            scanf("%d", &entradas[i]); 
-        
         }
     
     printf("Entre os pesos \n");  
     
     for(i=0;i<10;i++){
-        
-         
         scanf("%d", &pesos[i]); 
-        
     }
     
     printf("Digite a constante T\n");
@@ -53,9 +42,10 @@ int main(){
     
     
     if(resultado==0){
-    printf("\nNeuronio inibido!");}
-    else{printf("\nNeuronio ativado");}
-    
+    	printf("\nNeuronio inibido!");
+	} else{
+		printf("\nNeuronio ativado");
+	}
     
   return 0;
   
